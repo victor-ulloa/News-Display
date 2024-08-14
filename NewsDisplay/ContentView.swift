71d107bbc02f0ca433969c79a99d4877b92f2d87
Victor Ulloa <alejandroekas@gmail.com>
2024-08-13 23:56:33 -0400

    Initial Commit

---
+++ NewsDisplay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsDisplay
+//
+//  Created by Victor Ulloa on 2024-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsDisplay/NewsDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsDisplayApp.swift
+//  NewsDisplay
+//
+//  Created by Victor Ulloa on 2024-08-13.
+//
+
+import SwiftUI
+
+@main
+struct NewsDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
